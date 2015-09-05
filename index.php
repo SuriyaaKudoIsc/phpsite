c4482f48409f9cb9e070f19eb37fe7d6f3eedcaf
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-09-05 14:14:23 +0200

    +!!ASSETS!!; -style.css

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ echo "<center>Sorry. The Page ".$_REQUEST['page']." doesn't exist!</center>";
 }
 else
 $page_content = file_get_contents("pages/main.html");
-$page_content = file_get_contents("assets/style.css");
 $page_content = str_replace("!!HEADER!!", file_get_contents("include/header.html"),$page_content);
 $page_content = str_replace("!!LEFT_COLUMN!!", file_get_contents("include/left_column.html"),$page_content);
 $page_content = str_replace("!!RIGHT_COLUMN!!", file_get_contents("include/right_column.html"),$page_content);
 $page_content = str_replace("!!FOOTER!!", file_get_contents("include/footer.html"),$page_content);
 $page_content = str_replace("!!COMMON_TAGS!!", file_get_contents("include/common_tags.html"),$page_content);
+$page_content = str_replace("!!ASSETS!!", file_get_contents("include/assets.html"),$page_content);
 
 echo $page_content;
 
